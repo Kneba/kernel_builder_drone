543fcf493893283b2de581bd7eb7991131dbe156
Cliquers <abenkenari@gmail.com>
2021-10-29 23:41:28 +0700

    switch to venom-clang

--- build.sh
+++ build.sh
@@ -65,13 +65,13 @@ BUILD_TYPE="Nightly"
 
 # Specify compiler.
 # 'clang' or 'clangxgcc' or 'gcc' or 'gcc49'
-COMPILER=gcc49
+COMPILER=clang
 
 # Message on anykernel when installation
 MESSAGE="don't blame me if u get poor battery backup or weak performance . i'm not responsible . Do with Your Own Risk."
 
 # Kernel is LTO
-LTO=1
+LTO=0
 
 # Specify linker.
 # 'ld.lld'(default)
