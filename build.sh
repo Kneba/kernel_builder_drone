677989ad70c7a0ba285cffc1590774923b7ee49d
Cliquers <abenkenari@gmail.com>
2021-10-30 16:31:55 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -197,7 +197,7 @@ setversioning() {
 exports() {
 	export KBUILD_BUILD_USER="LetThereBe"
     export KBUILD_BUILD_HOST="Carnage"
-    export KBUILD_BUILD_VERSION="2"
+    export KBUILD_BUILD_VERSION="3"
 	export ARCH=arm64
 	export SUBARCH=arm64
 
