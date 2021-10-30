f3d3ecdf035e59addea9e62dc74a9af3f4900166
Cliquers <abenkenari@gmail.com>
2021-10-30 16:05:23 +0700

    asdasad

--- build.sh
+++ build.sh
@@ -55,7 +55,7 @@ DEFCONFIG=X00TD_defconfig
 MANUFACTURERINFO="ASUSTek Computer Inc."
 
 # Kernel Variant
-NAMA=FOX
+NAMA=TOM
 
 JENIS=HMP
 
@@ -197,7 +197,7 @@ setversioning() {
 exports() {
 	export KBUILD_BUILD_USER="LetThereBe"
     export KBUILD_BUILD_HOST="Carnage"
-    export KBUILD_BUILD_VERSION="3"
+    export KBUILD_BUILD_VERSION="1"
 	export ARCH=arm64
 	export SUBARCH=arm64
 
