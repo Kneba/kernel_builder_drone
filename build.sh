d116a7320b3a074a4eba6424ea098348d26d1ee7
Kneba <abenkenari@gmail.com>
2021-11-28 23:15:49 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -196,7 +196,7 @@ setversioning() {
 exports() {
 	export KBUILD_BUILD_USER="LetThereBe"
     export KBUILD_BUILD_HOST="Carnage"
-    export KBUILD_BUILD_VERSION="1"
+    export KBUILD_BUILD_VERSION="15"
 	export ARCH=arm64
 	export SUBARCH=arm64
 
