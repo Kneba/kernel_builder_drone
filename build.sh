48fba6b7ae1f9c76d1e30d0b7f0f5e560547771a
Kneba <abenkenari@gmail.com>
2021-12-03 12:10:35 +0700

    rm som

--- build.sh
+++ build.sh
@@ -196,7 +196,7 @@ setversioning() {
 exports() {
 	export KBUILD_BUILD_USER="LetThereBe"
     export KBUILD_BUILD_HOST="Carnage"
-    export KBUILD_BUILD_VERSION="56"
+    export KBUILD_BUILD_VERSION="10"
 	export ARCH=arm64
 	export SUBARCH=arm64
 
