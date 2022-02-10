6cf35790ff234c1f4ed6670dbdc49daa73316a36
Kneba <abenkenary3@gmail.com>
2022-02-11 01:35:51 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -57,7 +57,7 @@ MANUFACTURERINFO="ASUSTek Computer Inc."
 
 # Kernel Variant
 NAMA=TheOneMemory
-JENIS=EAS
+JENIS=HMP
 VARIAN=LV
 CODENAME=Hayzel
 
@@ -173,7 +173,7 @@ DATE2=$(TZ=Asia/Bangkok date +"%Y%m%d")
 		GCC32_DIR=$KERNEL_DIR/gcc32
 
 	msg "|| Cloning Anykernel ||"
-        git clone https://github.com/Kneba/AnyKernel3.git -b eas AnyKernel3
+        git clone https://github.com/Kneba/AnyKernel3.git -b tom AnyKernel3
 
 	if [ $BUILD_DTBO = 1 ]
 	then
