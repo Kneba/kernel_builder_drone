3d39eeca5af1ffceb27d2d64df385f27dea5e4df
Kneba <abenkenary3@gmail.com>
2022-02-19 20:17:29 +0700

    ew rebase from ruega

--- build.sh
+++ build.sh
@@ -55,16 +55,16 @@ DEFCONFIG=X00TD_defconfig
 MANUFACTURERINFO="ASUSTek Computer Inc."
 
 # Kernel Variant
-NAMA=TheOneMemory
+NAMA=EW
 JENIS=HMP
-VARIAN=Hayzel
+VARIAN=Siren
 
 # Build Type
 BUILD_TYPE="Nightly"
 
 # Specify compiler.
 # 'clang' or 'clangxgcc' or 'gcc' or 'gcc49'
-COMPILER=clang
+COMPILER=gcc49
 
 # Message on anykernel when installation
 MESSAGE="don't blame me if u get poor battery backup or weak performance . i'm not responsible . Do with Your Own Risk."
@@ -171,7 +171,7 @@ DATE2=$(TZ=Asia/Jakarta date +"%Y%m%d")
 		GCC32_DIR=$KERNEL_DIR/gcc32
 
 	msg "|| Cloning Anykernel ||"
-        git clone https://github.com/Kneba/Anykernel3.git -b tom AnyKernel3
+        git clone https://github.com/Tiktodz/Anykernel3.git -b main AnyKernel3
 
 	if [ $BUILD_DTBO = 1 ]
 	then
