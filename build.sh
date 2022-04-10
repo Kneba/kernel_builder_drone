ff026908d9bc70498b1e76394a7a17b216140135
Kneba <abenkenary3@gmail.com>
2022-04-10 23:40:22 +0700

    test port charge

--- build.sh
+++ build.sh
@@ -194,7 +194,7 @@ setversioning() {
 ##--------------------------------------------------------------##
 
 exports() {
-    export KBUILD_BUILD_USER="batman"
+    export KBUILD_BUILD_USER="queen"
     export KBUILD_BUILD_VERSION="1"
     export ARCH=arm64
     export SUBARCH=arm64
