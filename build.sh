44b6dba58c57339f1813f40f07598b585184e466
Kneba <abenkenary3@gmail.com>
2022-08-02 02:16:36 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -140,7 +140,7 @@ DATE2=$(TZ=Asia/Jakarta date +"%Y%m%d")
 	if [ $COMPILER = "clang" ]
 	then
 		msg "|| Cloning toolchain ||"
-		git clone --depth=1 https://github.com/Rasenkai/proton-clang -b master clang
+		git clone --depth=1 https://gitlab.com/Panchajanya1999/azure-clang.git clang
 
 	elif [ $COMPILER = "gcc49" ]
 	then
