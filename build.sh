b6677abdb8bda826cc9df524405751e6a11a4271
Kneba <abenkenary3@gmail.com>
2022-08-08 04:27:20 +0700

    Add jenis

--- build.sh
+++ build.sh
@@ -185,7 +185,7 @@ DATE2=$(TZ=Asia/Jakarta date +"%Y%m%d")
 # Function to replace defconfig versioning
 setversioning() {
     # For staging branch
-    KERNELNAME="$NAMA-$VARIAN-$LINUXVER"
+    KERNELNAME="$NAMA-$JENIS-$VARIAN-$LINUXVER"
     # Export our new localversion and zipnames
     export KERNELNAME
     export ZIPNAME="$KERNELNAME.zip"
