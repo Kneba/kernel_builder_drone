d0aa7760c29a1b5b1f021b26e57531eea24d3ac7
Kneba <abenkenary3@gmail.com>
2022-09-29 11:49:42 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -64,13 +64,13 @@ BUILD_TYPE="PORT"
 
 # Specify compiler.
 # 'clang' or 'clangxgcc' or 'gcc' or 'gcc49'
-COMPILER=clang
+COMPILER=gcc
 
 # Message on anykernel when installation
 MESSAGE="don't blame me if u get poor battery backup or weak performance . i'm not responsible . Do with Your Own Risk."
 
 # Kernel is LTO
-LTO=0
+LTO=1
 
 # Specify linker.
 # 'ld.lld'(default)
@@ -195,7 +195,7 @@ setversioning() {
 
 exports() {
     export KBUILD_BUILD_USER="queen"
-    export KBUILD_BUILD_VERSION="2"
+    export KBUILD_BUILD_VERSION="1"
     export ARCH=arm64
     export SUBARCH=arm64
 
