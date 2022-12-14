8b4867b66b6e8917ebe661da2e1287f2eb397428
Kneba <abenkenary3@gmail.com>
2022-12-14 22:46:34 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -56,15 +56,15 @@ MANUFACTURERINFO="ASUSTek Computer Inc."
 
 # Kernel Variant
 NAMA=TheOneMemory
-JENIS=NLV
+JENIS=LV
 VARIAN=HMP
 
 # Build Type
-BUILD_TYPE="PORT"
+BUILD_TYPE="thirteen"
 
 # Specify compiler.
 # 'clang' or 'clangxgcc' or 'gcc' or 'gcc49'
-COMPILER=gcc
+COMPILER=clang
 
 # Message on anykernel when installation
 MESSAGE="don't blame me if u get poor battery backup or weak performance . i'm not responsible . Do with Your Own Risk."
@@ -140,7 +140,7 @@ DATE2=$(TZ=Asia/Jakarta date +"%Y%m%d")
 	if [ $COMPILER = "clang" ]
 	then
 		msg "|| Cloning toolchain ||"
-		git clone --depth=1 https://github.com/Rasenkai/proton-clang.git -b master clang
+		git clone --depth=1 https://github.com/kdrag0n/proton-clang.git -b master clang
 
 	elif [ $COMPILER = "gcc49" ]
 	then
